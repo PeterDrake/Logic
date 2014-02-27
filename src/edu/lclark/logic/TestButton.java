393a920cbeccc148ec989220f41293cb5e0baf28
Rudy Reeves <rudyreeves@lclark.edu>
2014-02-27 14:59:18 -0800

    Modified classpath

--- src/edu/lclark/logic/TestButton.java
+++ src/edu/lclark/logic/TestButton.java
@@ -3,9 +3,7 @@ package edu.lclark.logic;
 import java.awt.EventQueue;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
-
 import javax.swing.JFrame;
-
 
 public class TestButton {
 
