96110c0879fce747a29d53bbc704aea2516537e8
Rudy Reeves <rudyreeves@lclark.edu>
2014-03-04 15:26:49 -0800

    Added an "Add Column" button and changed JLabels to JTextFields

--- src/edu/lclark/logic/TruthTable.java
+++ src/edu/lclark/logic/TruthTable.java
@@ -1,18 +1,22 @@
 package edu.lclark.logic;
 
-/** A truth table for a well-formed formula */
+import java.util.ArrayList;
+import java.util.List;
+
+/** A truth table model for a well-formed formula */
 public class TruthTable implements View {
     /** The well-formed formula this truth table represents */
-	final String formula;
+	final private String formula;
 
 	/** The letters found in the formula */
-	final char[] letters = new char[5];
+	final private char[] letters = new char[5];
 
 	/** The number of letters in the formula */
-	int numLetters;
+	private int numLetters;
 
 	/** The set of all possible combinations of truth-values for each letter in the formula */
-	boolean[][] truthValues;
+	private boolean[][] truthValues;
+	private List<TruthTableColumn> columns;
 
 	/** Constructor; takes target formula */
 	public TruthTable(String formula) {
@@ -31,9 +35,24 @@ public class TruthTable implements View {
 	    return letters[i];
 	}
 
-	/** Returns the number of rows in the table: 2 ^ numLetters */
+	/** Returns the number of rows (not counting the "label" row in the GUI) in the table: 2 ^ numLetters */
 	public int getNumRows() {
 	    return 1 << numLetters; // same as: (int) Math.pow(2, numLetters)
+	}
+
+	/** Returns the ith column in the truth table, as a TruthTableColumn. */
+	public TruthTableColumn getColumn(int col) {
+	    return columns.get(col);
+	}
+
+	/** Returns the number of columns in the truth table. */
+	public int getNumColumns() {
+	    return columns.size();
+	}
+
+	/** Adds a column to the right side of the truth table. */
+	public void addColumn(TruthTableColumn column) {
+	    columns.add(column);
 	}
 
 	/** Returns how many letters are in target formula */
@@ -51,19 +70,22 @@ public class TruthTable implements View {
 	}
 
 	/** Initializes truth-table with each truth-value combination of the letters */
-	public void initTable() {
+	private void initTable() {
 	    int numRows = getNumRows();
 	    truthValues = new boolean[numRows][numLetters];
+	    columns = new ArrayList<TruthTableColumn>(numLetters);
 	    // For each column...
 	    for (int col = 0; col < numLetters; col++) {
 	        int numPartitions = 1 << (col + 1); // same as: (int) Math.pow(2, col+1)
 	        int partionLength = numRows / numPartitions;
+	        columns.add(new TruthTableColumn("" + letters[col], new boolean[numRows]));
 	        // ...for each partition in the column...
 	        for (int p = 0; p < numPartitions - 1; p += 2) {
 	            int start = p * partionLength;
 	            // ...for each row in that partition:
 	            for (int row = 0; row < partionLength; row++) {
 	                truthValues[row + start][col] = true;
+	                columns.get(col).setValue(row + start + 1, true);
 	            }
 	        }
 	    }
