c42c326863d561e841426be5aa7927526896a640
James Fischer <jamesfischer@lclark.edu>
2014-03-04 15:59:47 -0800

    added TruthTableChecker.java class

---
+++ src/edu/lclark/logic/TruthTableChecker.java
@@ -0,0 +1,16 @@
+package edu.lclark.logic;
+
+public class TruthTableChecker {
+
+	private String formula ;
+	
+	private boolean[] truthValues ;
+	
+	public TruthTableChecker(String formula, boolean[] truthValues) {
+		this.formula = formula ;
+		this.truthValues = truthValues ;
+		
+		
+	}
+	
+}
