a72dd9e219c1667506860b959141080d68d14453
James Fischer <jamesfischer@lclark.edu>
2014-03-06 14:33:45 -0800

    added tables of correct values conjunction and disjunction

--- src/edu/lclark/logic/TruthTableChecker.java
+++ src/edu/lclark/logic/TruthTableChecker.java
@@ -1,16 +1,55 @@
 package edu.lclark.logic;
 
+import java.math.*;
+
 public class TruthTableChecker {
 
-	private String formula ;
-	
-	private boolean[] truthValues ;
-	
+	private String formula;
+
+	private boolean[] truthValues;
+	private boolean[] matches;
+
 	public TruthTableChecker(String formula, boolean[] truthValues) {
-		this.formula = formula ;
-		this.truthValues = truthValues ;
+		this.formula = formula;
+		this.truthValues = truthValues;
+		matches = new boolean[truthValues.length];
+	}
+
+	public boolean[] compare(boolean[] enteredValues) {
+		for (int i = 0; i < truthValues.length; i++) {
+			if (enteredValues[i] == truthValues[i]) {
+				matches[i] = true;
+			} else {
+				matches[i] = false;
+			}
+		}
+		return matches;
+	}
+
+	public boolean[] conjunction(int numLetters, boolean[][] allPossibleValues,
+			int letter1, int letter2) {
+		boolean[] values = new boolean[(int) Math.pow(2, numLetters)];
+		for (int i = 0; i < values.length; i++) {
+			if (allPossibleValues[letter1][i] == true
+					&& allPossibleValues[letter2][i] == true) {
+				values[i] = true;
+			}
+		}
+
+		return values;
+	}
+	public boolean[] disjunction(int numLetters, boolean[][] allPossibleValues,
+			int letter1, int letter2) {
+		boolean[] values = new boolean[(int) Math.pow(2, numLetters)];
+		for (int i = 0; i < values.length; i++) {
+			if (allPossibleValues[letter1][i] == true
+					|| allPossibleValues[letter2][i] == true) {
+				values[i] = true;
+			}
+		}
 		
-		
+		return values;
 	}
 	
+
 }
---
+++ src/edu/lclark/logic/TruthTableCheckerTest.java
@@ -0,0 +1,49 @@
+package edu.lclark.logic;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TruthTableCheckerTest {
+
+	boolean[] set1 = { true, true, false };
+	TruthTableChecker checker = new TruthTableChecker("p q", set1);
+
+	@Test
+	public void compareTest() {
+		boolean[] set2 = { true, true, false };
+		boolean[] matches = { true, true, true };
+		// System.out.println(checker.compare(set2).toString());
+		for (int i = 0; i < matches.length; i++) {
+			assertEquals(matches[i], checker.compare(set2)[i]);
+		}
+		set2[2] = true;
+		matches[2] = false;
+		for (int i = 0; i < matches.length; i++) {
+			assertEquals(matches[i], checker.compare(set2)[i]);
+		}
+
+	}
+
+	@Test
+	public void conjunctionTest() {
+		boolean[][] letterValues = { { true, false, true, false },
+				{ true, true, false, false } };
+		boolean[] correctValues = { true, false, false, false };
+		for (int i = 0; i < correctValues.length; i++) {
+			assertEquals(correctValues[i],
+					checker.conjunction(2, letterValues, 0, 1)[i]);
+		}
+	}
+
+	@Test
+	public void disjunctionTest() {
+		boolean[][] letterValues = { { true, false, true, false },
+				{ true, true, false, false } };
+		boolean[] correctValues = { true, true, true, false };
+		for (int i = 0; i < correctValues.length; i++) {
+			assertEquals(correctValues[i],
+					checker.disjunction(2, letterValues, 0, 1)[i]);
+		}
+	}
+}
