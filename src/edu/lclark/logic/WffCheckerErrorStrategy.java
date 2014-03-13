21958a764462ead71d83da03813e2bb9d37860de
Spencer Wuthmann <swuthmann@lclark.edu>
2014-03-13 13:14:28 -0700

    added comments to list error messages

--- src/edu/lclark/logic/WffCheckerErrorStrategy.java
+++ src/edu/lclark/logic/WffCheckerErrorStrategy.java
@@ -7,23 +7,27 @@ public class WffCheckerErrorStrategy extends DefaultErrorStrategy {
 
 	@Override
 	public void recover(Parser recognizer, RecognitionException e) {
-		for (ParserRuleContext context = recognizer.getContext(); context != null; context = context.getParent()) {
+		for (ParserRuleContext context = recognizer.getContext(); context != null; context = context
+				.getParent()) {
 			context.exception = e;
 		}
-
 		throw new ParseCancellationException(e);
 	}
 
 	@Override
-	public Token recoverInline(Parser recognizer)
-			throws RecognitionException
-			{
+	public Token recoverInline(Parser recognizer) throws RecognitionException {
 		InputMismatchException e = new InputMismatchException(recognizer);
-		for (ParserRuleContext context = recognizer.getContext(); context != null; context = context.getParent()) {
+		for (ParserRuleContext context = recognizer.getContext(); context != null; context = context
+				.getParent()) {
 			context.exception = e;
 		}
-
 		throw new ParseCancellationException(e);
-			}
+	}
+	
+//	Recognition Exception:
+//	No Viable Alt Exception:
+//	Lexer No Viable Alt Exception:
+//	Input Mismatch Exception:
+//	Failed Predicate Exception:
 
 }
