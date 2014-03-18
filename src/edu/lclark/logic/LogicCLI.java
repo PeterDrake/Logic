60a19a0c5ca72b970b07415bd51a72a660aaafec
Kekai Clarke <kclarke@lclark.edu>
2014-03-18 15:10:50 -0700

    altered hilit functionality

--- src/edu/lclark/logic/LogicCLI.java
+++ src/edu/lclark/logic/LogicCLI.java
@@ -6,31 +6,32 @@ import asg.cliche.Command;
 import asg.cliche.ShellFactory;
 
 public class LogicCLI implements View {
-	
+
 	private WffChecker wc;
-	
+
 	@Command
 	public void wff(String s) {
 		System.out.println(wc.setInputString(s));
 	}
-	
+
 	@Command
 	public void tree(String s) {
 		wc.setInputString(s);
 		System.out.println(wc.printTree());
 	}
-	
+
 	@Command
 	public void gui(String s) {
 		wc.setInputString(s);
 		wc.guiTree();
 	}
-	
+
 	public LogicCLI() {
 		wc = new WffChecker();
 	}
-	
-	public static void main(String[] args) throws IOException  {
-		ShellFactory.createConsoleShell("wffchecker", "", new LogicCLI()).commandLoop();
-    }
+
+	public static void main(String[] args) throws IOException {
+		ShellFactory.createConsoleShell("wffchecker", "", new LogicCLI())
+				.commandLoop();
+	}
 }
