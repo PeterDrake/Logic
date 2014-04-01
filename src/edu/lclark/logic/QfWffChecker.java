40615f7b665ff0196c061dea25bfde64a4b175ad
Kaleb Ganz <kganz@lclark.edu>
2014-04-01 14:54:18 -0700

    added back two deleted files

---
+++ src/edu/lclark/logic/QfWffChecker.java
@@ -0,0 +1,46 @@
+package edu.lclark.logic;
+
+import org.antlr.v4.runtime.*;
+
+public class QfWffChecker extends WffChecker {
+		
+	// Basically a constructor, but because of the way
+	// ANTLR works we can't do that unless we pass a string
+	// to the constructor which is not optimal
+	
+	public QfWffChecker(String is) {
+		setInput(new ANTLRInputStream(is));
+		setLexer(new QfWffLexer(getInput())); 
+		setTokens(new CommonTokenStream(getLexer())); 
+		setParser(new QfWffParser(getTokens()));
+		
+		super.swapParserErrorHandling(getParser());
+	}
+	
+	public boolean checkWff() {
+		// There will be a RuntimeException if there is invalid syntax, so we catch it 
+		try {
+			setTree(((QfWffParser) getParser()).formula());
+		}
+		catch (RuntimeException re) {
+			return false;
+		}
+
+		// a little awkward, but there is one weird case where paren checking is messed up
+		if (getErrors() != "The entered formula is a wff.") {
+			return false;
+		}
+
+		return true;
+	}
+	
+	// Super basic test
+	public static void main(String[] args) {
+//		QfWffChecker qfwc = new QfWffChecker("(∀x) (Fx) -> (Gx)");
+		QfWffChecker qfwc = new QfWffChecker("(∀x)(∃y)(Fx,y)");
+		System.out.println(qfwc.checkWff());
+		System.out.println(qfwc.printTree());
+		System.out.println(qfwc.getErrors());
+	}
+}
+
