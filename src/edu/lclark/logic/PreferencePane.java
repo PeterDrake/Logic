41702d1e4403a967881449b52b6eb7ad514f53a4
Miles Crabill <miles@milescrabill.com>
2014-04-24 14:48:17 -0700

    PreferencePane working properly

--- src/edu/lclark/logic/PreferencePane.java
+++ src/edu/lclark/logic/PreferencePane.java
@@ -2,6 +2,7 @@ package edu.lclark.logic;
 
 import java.awt.*;
 import java.awt.event.*;
+import java.util.Enumeration;
 
 import javax.swing.*;
 
@@ -13,6 +14,7 @@ public class PreferencePane extends JFrame {
     public static final int DEFAULT_HEIGHT = 300;
     
     private String[] symbols;
+    private JRadioButton[][] buttons = new JRadioButton[5][];
         
     private JPanel createRow(String title, JPanel panel) {
 		JPanel newPanel = new JPanel(new GridLayout(1, 2));
@@ -21,22 +23,24 @@ public class PreferencePane extends JFrame {
     	return newPanel;
     }
     
-    private JPanel makeButtonPanel(JButton[] buttons) {
+    private JPanel makeButtonPanel(JRadioButton[] buttons) {
     	JPanel newPanel = new JPanel(new GridLayout(1, buttons.length));
-    	for(JButton button : buttons) {
+    	for(JRadioButton button : buttons) {
     		newPanel.add(button);
     	}
     	return newPanel;
     }
     
-    private JButton[] makeButtons(String[] strings, int row) {
-    	JButton[] buttons = new JButton[strings.length];
+    private JRadioButton[] makeButtons(String[] strings, int row) {
+    	ButtonGroup group = new ButtonGroup();
+    	JRadioButton[] buttons = new JRadioButton[strings.length];
     	int i = 0;
     	for(String string : strings) {
-    		JButton button = new JButton(string);
-    		button.addActionListener(new ButtonPressAction(row, string));
+    		JRadioButton button = new JRadioButton(string);
+    		group.add(button);
     		buttons[i++] = button;
     	}
+    	this.buttons[row] = buttons;
     	return buttons;
     }
     
@@ -44,45 +48,61 @@ public class PreferencePane extends JFrame {
     	// TODO add default symbols
     	// TODO add submit and/or save button for preferences
     	symbols = new String[] {"↔", "→", ".", "⋁", "-"};
-    }
-    
-    public void newWindow() {
+    	
     	JPanel panel = new JPanel();
-    	panel.setLayout(new GridLayout(5, 1));
+    	panel.setLayout(new GridLayout(6, 1));
     	
     	panel.add(createRow("biconditional", makeButtonPanel(makeButtons(new String[]{"↔","<->","≡"}, 0))));
     	panel.add(createRow("conditional", makeButtonPanel(makeButtons(new String[]{"→","->","⊃"}, 1))));
     	panel.add(createRow("conjunction", makeButtonPanel(makeButtons(new String[]{".","&","^"}, 2))));
-    	panel.add(createRow("inclusive or", makeButtonPanel(makeButtons(new String[]{"v","⋁"}, 3))));
+    	panel.add(createRow("inclusive or", makeButtonPanel(makeButtons(new String[]{"⋁","v"}, 3))));
     	panel.add(createRow("negation", makeButtonPanel(makeButtons(new String[]{"-","¬", "~"}, 4))));
 
+    	JButton submitButton = new JButton("Submit");
+    	submitButton.addActionListener(new SubmitButtonAction());
+    	panel.add(submitButton);
+    	
     	this.add(panel);
     	
-		this.pack();
-		this.setVisible(true);
     	this.setTitle("Preferences");
     }
     
-	private class ButtonPressAction extends AbstractAction {
-		/**
-		 * 
-		 */
-		private static final long serialVersionUID = 1L;
-
-		String symbol;
-		int i;
-		
-		public ButtonPressAction(int i, String symbol) {
-			this.i = i;
-			this.symbol = symbol;
+    public void newWindow() {    
+    	
+		for (int row = 0; row < buttons.length; row++) {
+			for (JRadioButton c : buttons[row]) {
+				if (symbols[row] == c.getText()) {
+					c.setSelected(true);;
+				}
+			}
+//			System.out.println(symbols[row]);
 		}
+    	
+		this.pack();
+		this.setVisible(true);
+    }
+    
+    private void hideWindow() {
+    	this.setVisible(false);
+    }
+    
+	private class SubmitButtonAction extends AbstractAction {
+		
+		private static final long serialVersionUID = 1L;
 		
 		public void actionPerformed(ActionEvent event) {
-			symbols[i] = symbol;
-//			System.out.println(i + " " + symbol);
+			for (int row = 0; row < buttons.length; row++) {
+				for (JRadioButton c : buttons[row]) {
+					if (c.isSelected()) {
+						symbols[row] = c.getText();
+					}
+				}
+//				System.out.println(symbols[row]);
+			}
+			hideWindow();
 		}
 	}
-	
+
 	public String[] getSymbols() {
 		return symbols;
 	}
@@ -93,5 +113,6 @@ public class PreferencePane extends JFrame {
     	for (String string : test) {
     		System.out.println(string);
     	}
+    	pp.newWindow();
     }
 }
